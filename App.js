6d72fac5acfdde1d92a7dcf29f8ba3e6fc532cc8
SURYA NARAYANAN K S <108388181+Surya152002@users.noreply.github.com>
2024-04-23 03:40:20 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EEGData from './EEGData';
+
+function App() {
+  return (
+    <div className="App">
+      <EEGData />
+    </div>
+  );
+}
+
+export default App;
