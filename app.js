c4da5d9dcca6940f947111a688d5effe8372455c
SURYA NARAYANAN K S <108388181+Surya152002@users.noreply.github.com>
2024-04-24 19:44:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-var gn = new GyroNorm();
+// Get the gyroscope data
+window.addEventListener('deviceorientation', function(event) {
+  var alpha = event.alpha;
+  var beta = event.beta;
+  var gamma = event.gamma;
 
-gn.init({
-  frequency: 50,
-  gravityNormalized: true,
-  orientationBase: GyroNorm.GAME,
-  decimalCount: 0, // Set decimalCount to 0 to get whole numbers
-  logger: null,
-  screenAdjusted: false
-}).then(function() {
-  gn.start(function(data) {
-    var gyroX = Math.round(data.do.gamma);
-    var gyroY = Math.round(data.do.beta);
-    var gyroZ = Math.round(data.do.alpha);
-    var accX = Math.round(data.do.ax);
-    var accY = Math.round(data.do.ay);
-    var accZ = Math.round(data.do.az);
+  document.getElementById("gyroX").innerHTML = Math.round(alpha);
+  document.getElementById("gyroY").innerHTML = Math.round(beta);
+  document.getElementById("gyroZ").innerHTML = Math.round(gamma);
+});
 
-    document.getElementById("gyroX").innerHTML = gyroX;
-    document.getElementById("gyroY").innerHTML = gyroY;
-    document.getElementById("gyroZ").innerHTML = gyroZ;
-    document.getElementById("accX").innerHTML = accX;
-    document.getElementById("accY").innerHTML = accY;
-    document.getElementById("accZ").innerHTML = accZ;
-  });
+// Get the accelerometer data
+window.addEventListener('devicemotion', function(event) {
+  var accelerationX = event.acceleration.x;
+  var accelerationY = event.acceleration.y;
+  var accelerationZ = event.acceleration.z;
+
+  document.getElementById("accX").innerHTML = Math.round(accelerationX);
+  document.getElementById("accY").innerHTML = Math.round(accelerationY);
+  document.getElementById("accZ").innerHTML = Math.round(accelerationZ);
 });
