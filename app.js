456c5e6c7785de3bc11361c7f406f420d695a455
SURYA NARAYANAN K S <108388181+Surya152002@users.noreply.github.com>
2024-04-24 18:26:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 function updateGyroscopeData(event) {
-  document.getElementById('gyroscope-x-value').textContent = event.gamma;
-  document.getElementById('gyroscope-y-value').textContent = event.beta;
-  document.getElementById('gyroscope-z-value').textContent = event.alpha;
+  document.getElementById('gyroscope-x-value').textContent = Math.round(event.gamma * 100) / 100;
+  document.getElementById('gyroscope-y-value').textContent = Math.round(event.beta * 100) / 100;
+  document.getElementById('gyroscope-z-value').textContent = Math.round(event.alpha * 100) / 100;
 }
 
 function updateAccelerometerData(event) {
-  document.getElementById('accelerometer-x-value').textContent = event.accelerationIncludingGravity.x;
-  document.getElementById('accelerometer-y-value').textContent = event.accelerationIncludingGravity.y;
-  document.getElementById('accelerometer-z-value').textContent = event.accelerationIncludingGravity.z;
+  document.getElementById('accelerometer-x-value').textContent = Math.round(event.accelerationIncludingGravity.x * 100) / 100;
+  document.getElementById('accelerometer-y-value').textContent = Math.round(event.accelerationIncludingGravity.y * 100) / 100;
+  document.getElementById('accelerometer-z-value').textContent = Math.round(event.accelerationIncludingGravity.z * 100) / 100;
 }
 
 window.addEventListener('deviceorientation', updateGyroscopeData);
