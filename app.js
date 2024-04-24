5f4fe819103248bab7e188b26004679afdff6a14
SURYA NARAYANAN K S <108388181+Surya152002@users.noreply.github.com>
2024-04-24 18:11:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const socket = new WebSocket('ws://localhost:3000');
+function updateGyroscopeData(event) {
+  document.getElementById('gyroscope-x-value').textContent = event.gamma;
+  document.getElementById('gyroscope-y-value').textContent = event.beta;
+  document.getElementById('gyroscope-z-value').textContent = event.alpha;
+}
 
-socket.addEventListener('open', (event) => {
-  console.log('WebSocket connection opened:', event);
-});
+function updateAccelerometerData(event) {
+  document.getElementById('accelerometer-x-value').textContent = event.accelerationIncludingGravity.x;
+  document.getElementById('accelerometer-y-value').textContent = event.accelerationIncludingGravity.y;
+  document.getElementById('accelerometer-z-value').textContent = event.accelerationIncludingGravity.z;
+}
 
-socket.addEventListener('message', (event) => {
-  const data = JSON.parse(event.data);
-  document.getElementById('gyroscope-x-value').textContent = data.gyroscope.x;
-  document.getElementById('gyroscope-y-value').textContent = data.gyroscope.y;
-  document.getElementById('gyroscope-z-value').textContent = data.gyroscope.z;
-  document.getElementById('accelerometer-x-value').textContent = data.accelerometer.x;
-  document.getElementById('accelerometer-y-value').textContent = data.accelerometer.y;
-  document.getElementById('accelerometer-z-value').textContent = data.accelerometer.z;
-});
-
-socket.addEventListener('close', (event) => {
-  console.log('WebSocket connection closed:', event);
+window.addEventListener('deviceorientation', updateGyroscopeData);
+window.addEventListener('devicemotion', updateAccelerometerData);
